0a025491d50318d94f1e2b22ab3347d8cd7226c9
Wojciech Król <wojciechkrol@MacBook-Air-Wojciech.local>
2014-08-30 16:15:02 +0200

    Initial Commit

---
+++ ObjCtFtp/ObjCtFtp.h
@@ -0,0 +1,13 @@
+//
+//  ObjCtFtp.h
+//  ObjCtFtp
+//
+//  Created by Wojciech Król on 30.08.2014.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ObjCtFtp : NSObject
+
+@end
---
+++ ObjCtFtp/ObjCtFtp.m
@@ -0,0 +1,13 @@
+//
+//  ObjCtFtp.m
+//  ObjCtFtp
+//
+//  Created by Wojciech Król on 30.08.2014.
+//
+//
+
+#import "ObjCtFtp.h"
+
+@implementation ObjCtFtp
+
+@end
